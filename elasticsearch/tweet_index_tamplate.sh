56c2643721f59e8340b1b0508ba740b47800d621
nicolajkirchhof <nicolaj.kirchhof@zuehlke.com>
2016-03-05 14:32:04 +0100

    Initial checkin

---
+++ elasticsearch/tweet_index_tamplate.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+curl -XPUT localhost:9200/_template/tweet01 -d '{
+    "template" : "tweets*",
+    "settings" : {
+        "number_of_shards" : 1
+    },
+    "mappings" : {
+        "tweet_sentiment" : {
+            "_all":       { "enabled": true },
+            "_source":    { "enabled": true },
+            "properties": {
+                "date":         {
+                    "type": "date",
+                    "format": "yyyy-MM-dd HH:mm:ss||epoch_millis",
+                    "doc_values": true
+                },
+                "message":      {
+                    "type": "string",
+                    "fields": {
+                        "raw": {
+                            "type":  "string",
+                            "index": "not_analyzed"
+                        },
+                        "german": {
+                            "type":     "string",
+                            "analyzer": "german"
+                        }
+                    }
+                },
+                "language":     { "type": "string", "index": "not_analyzed" },
+                "hashtags":     { "type": "string", "index": "not_analyzed" },
+                "location":     { "type": "geo_point", "ignore_malformed" : true },
+                "sentiment":    { "type": "string", "index": "not_analyzed" },
+                "polarity":     { "type": "float" },
+                "subjectivity": { "type": "float" }
+            }
+        }
+    }
+}'
